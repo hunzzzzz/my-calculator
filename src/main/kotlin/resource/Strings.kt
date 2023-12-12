776c488c6317e9f8f83013511c7565d7930896c8
hunzz <hunzz.study@gmail.com>
2023-12-12 11:14:42 +0900

    change the structure of Strings.kt

--- src/main/kotlin/resource/Strings.kt
+++ src/main/kotlin/resource/Strings.kt
@@ -1,8 +1,10 @@
 package resource
 
-object Strings {
-    const val greeting: String = "========== My Calculator 에서 오신 것을 환영합니다. =========="
-    const val chooseOptions: String = "원하는 기능을 선택하세요. ([1] 덧셈, [2] 뺄셈, [3] 곱셈, [4] 나눗셈 [0] 계산기 종료) : "
-    const val wrongOption: String = "잘못된 값을 입력하셨습니다. 다시 입력해주세요."
-    const val getNumber: String = "번째 숫자를 입력하세요 : "
+class Strings {
+    companion object {
+        const val greeting: String = "========== My Calculator 에서 오신 것을 환영합니다. =========="
+        const val chooseOptions: String = "원하는 기능을 선택하세요. ([1] 덧셈, [2] 뺄셈, [3] 곱셈, [4] 나눗셈 [0] 계산기 종료) : "
+        const val wrongOption: String = "잘못된 값을 입력하셨습니다. 다시 입력해주세요."
+        const val getNumber: String = "번째 숫자를 입력하세요 : "
+    }
 }
