deeaf9856a9ee17883e48dd38e6437102403b0b4
hunzz <hunzz.study@gmail.com>
2023-12-12 10:59:33 +0900

    make Calculator.kt(start method) & Strings.kt

--- src/main/kotlin/operation/Calculator.kt
+++ src/main/kotlin/operation/Calculator.kt
@@ -1,7 +1,46 @@
 package operation
 
+import abstract.AbstractOperation
+import resource.Strings
+
 class Calculator {
 
-    fun start(){}
+    fun start() {
+        var option: String
+        var num1: Int
+        var num2: Int
+
+        println(Strings.greeting)
+        while (true) {
+            println(Strings.chooseOptions)
+
+            option = readln()
+            num1 = getTwoNumbers().first
+            num2 = getTwoNumbers().second
+
+            when (option) {
+                "0" -> return
+                "1" -> startOperation(num1, num2, AddOperation())
+                "2" -> startOperation(num1, num2, SubtractOperation())
+                "3" -> startOperation(num1, num2, MultiplyOperation())
+                "4" -> startOperation(num1, num2, DivideOperation())
+                else -> println("잘못된 값을 입력하셨습니다. 다시 입력해주세요.")
+            }
+        }
+    }
+
+    private fun startOperation(x: Int, y: Int, operation: AbstractOperation) {
+        operation.operate(x, y)
+    }
+
+    private fun getTwoNumbers(): Pair<Int, Int> {
+        print(Strings.getFirstNum)
+        val num1 = readln().toInt()
+
+        print(Strings.getSecondNum)
+        val num2 = readln().toInt()
+
+        return Pair(num1, num2)
+    }
 }
 
---
+++ src/main/kotlin/resource/Strings.kt
@@ -0,0 +1,8 @@
+package resource
+
+object Strings {
+    const val greeting: String = "========== My Calculator 에서 오신 것을 환영합니다. =========="
+    const val chooseOptions: String = "원하는 기능을 선택하세요. ([1] 덧셈, [2] 뺄셈, [3] 곱셈, [4] 나눗셈 [0] 계산기 종료) : "
+    const val getFirstNum: String = "첫 번째 숫자를 입력하세요 : "
+    const val getSecondNum: String = "두 번째 숫자를 입력하세요 : "
+}
